09fd5b2f0911dba47ffee657a5fa1ea995f8adb9
kmaksim2197 <kmaksim2197@gmail.com>
2025-12-16 15:41:25 +0500

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,11 @@
+INSERT INTO Talabalar VALUES (1, 'Ali', 3.5);
+INSERT INTO Talabalar VALUES (2, 'Vali', 3.8);
+INSERT INTO Talabalar VALUES (3, 'Hasan', 3.2);
+
+SELECT * FROM Mahsulotlar;
+
+SELECT nom, narx FROM Mahsulotlar;
+
+SELECT GPA FROM Talabalar;
+
+SELECT * FROM Xodimlar;
