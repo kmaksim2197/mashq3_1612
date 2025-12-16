cbd0fe8a1436640a4d1490c9554c344686d37aff
kmaksim2197 <kmaksim2197@gmail.com>
2025-12-16 15:42:15 +0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,3 +1,20 @@
+CREATE TABLE Talabalar (
+    id INTEGER PRIMARY KEY,
+    ism TEXT,
+    GPA REAL
+);
+
+CREATE TABLE Mahsulotlar (
+    nom TEXT,
+    narx REAL
+);
+
+CREATE TABLE Xodimlar (
+    id INTEGER,
+    ism TEXT,
+    lavozim TEXT
+);
+
 INSERT INTO Talabalar VALUES (1, 'Ali', 3.5);
 INSERT INTO Talabalar VALUES (2, 'Vali', 3.8);
 INSERT INTO Talabalar VALUES (3, 'Hasan', 3.2);
